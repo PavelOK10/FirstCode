173236f3a079ce0a105722c40f73314b85271bbf
PaRus <PaRusakovich@gmail.com>
2021-03-16 13:48:48 +0300

    Добавьте файлы проекта.

---
+++ Test1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+ 
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("Второе число больше первого");
+            }
+            else 
+            {
+                Console.WriteLine("Числа равны");
+            }
+        }
+    }
+}
